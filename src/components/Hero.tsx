c05158fa280cd5b1f5afceccaf536ab0b0c992d6
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-02 14:55:26 -0800

    fix background layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,22 +53,8 @@ export function Hero() {
                   <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
                     We’re changing the way people connect.
                   </h1>
-                  <p className="mt-6 text-lg leading-8 text-gray-400 sm:max-w-md lg:max-w-none">
-                    Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in quis cupidatat mollit aute velit. Et
-                    labore commodo nulla aliqua proident mollit ullamco exercitation tempor. Sint aliqua anim nulla sunt
-                    mollit id pariatur in voluptate cillum.
-                  </p>
-                  <div className="mt-10 flex items-center gap-x-6">
-                    <a
-                      href="#"
-                      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Get started
-                    </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                      Live demo <span aria-hidden="true">→</span>
-                    </a>
-                  </div>
+                  {/* this is where to put the input submit */}
+                  {/* this is wher to put the form */}
                 </div>
                 <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                   <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
