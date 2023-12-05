0484aa2116f9f72018b751cb9d6d42f36063f0ca
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-05 00:32:59 -0800

    add github page to show the results

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,60 +1,43 @@
 'use client'
-import { useRouter } from 'next/router';
-// import { useEffect, useState } from 'react';
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react";
 
-// export default function GithubStatsPage() {
-//     const router = useRouter();
-    // const { username } = router.query;
-    // const [loading, setLoading] = useState(true);
-    // const [data, setData] = useState(null);
+export default function Page() {
+  const [data, setData] = useState(null);
+  const searchParams = useSearchParams();
+  const username = searchParams.get("username")
+  
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/github-stats`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username }),
+        })
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+          console.log('Error fetching Github stats', error);
+      } finally {
+        // this is for setLoading state 
+      }
+    }
 
-    // useEffect(() => {
-    //     if (!username || Array.isArray(username)) return;
+    fetchData();
+    
+  }, [username])
 
-    //     const fetchData = async () => {
-    //         setLoading(true);
-    //         try {
-    //             const response = await fetch(`/api/github-stats`, {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                 },
-    //                 body: JSON.stringify({ username }),
-    //             });
-    //             if (!response.ok) {
-    //                 throw new Error('Network response was not ok');
-    //             }
-    //             const data = await response.json();
-    //             setData(data);
-    //         } catch (error) {
-    //             console.error('Error fetching GitHub stats:', error);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     };
-
-    //     fetchData();
-    // }, [username]);
-
-    // if (loading) {
-    //     return <div>Loading...</div>; // Replace with your loading graphics or component
-    // }
-
-    // if (!data) {
-    //     return <div>No data found</div>;
-    // }
-
-//     return (
-//         <div>
-//             {/* Display your data here */}
-//             <p>Hello</p>
-//         </div>
-//     );
-// }
-
-
-export default function ls() {
-    return(
-        <p>hello</p>
-    )
+  return(
+    <div>
+      <p>hello</p>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  )
 }
