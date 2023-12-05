5690983810288c6f01b498dcc9cb57f2b0c9494a
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-05 00:51:12 -0800

    idk what changed here

---
+++ src/components/GithubStatPage.tsx
@@ -0,0 +1,39 @@
+import { GetServerSideProps, NextPage } from 'next';
+
+// Define the type for the props
+interface GithubStatsPageProps {
+  data: any; // Replace 'any' with a more specific type based on the data structure you expect
+}
+
+export const GithubStatsPage: NextPage<GithubStatsPageProps> = ({ data }) => {
+  // Render your page with the fetched data
+  return (
+    <div>
+      {/* Display your data here */}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<GithubStatsPageProps> = async (context) => {
+  const username = context.query.username;
+
+  // Ensure username is a string
+  if (typeof username !== 'string') {
+    return { props: { data: null } };
+  }
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/github-stats?username=${username}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return { props: { data } };
+  } catch (error) {
+    console.error('Error fetching GitHub stats:', error);
+    // Handle error appropriately
+    return { props: { data: null } };
+  }
+};
+
