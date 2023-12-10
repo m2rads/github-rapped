e35433da1d70ef69b023940b72f0661db1f6bc81
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-09 20:08:41 -0800

    add dummy data

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+'use client'
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react";
 
 interface GitHubStats {
   totalJanuaryCommits: number;
@@ -14,9 +14,10 @@ interface GitHubStats {
 export default function Page() {
   const [data, setData] = useState<GitHubStats | null>(null);
   const searchParams = useSearchParams();
-  const username = searchParams.get('username');
-
+  const username = searchParams.get("username")
+  
   useEffect(() => {
+    
     const fetchData = async () => {
       try {
         const response = await fetch(`/api/github-stats`, {
@@ -25,34 +26,38 @@ export default function Page() {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({ username }),
-        });
+        })
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
         const data = await response.json();
         setData(data);
       } catch (error) {
-        console.log('Error fetching Github stats', error);
+          console.log('Error fetching Github stats', error);
       } finally {
-        // this is for setLoading state
+        // this is for setLoading state 
       }
-    };
+    }
 
     fetchData();
-  }, [username]);
+    
+  }, [username])
 
-  return (
+  return(
     <div>
       {data && (
         <>
-          <p>Total January Commits: {data.totalJanuaryCommits}</p>
-          <p>Threshold for Active Start: {data.threshold}</p>
-          <p>
-            Active Start to the Year:{' '}
-            {data.isActive ? 'true' : data.encouragingMessage}
-          </p>
+         <div className="firstSlide">
+            <p>Total January Commits: {data.totalJanuaryCommits}</p>
+            <p>Threshold for Active Start: {data.threshold}</p>
+            <p>Active Start to the Year: {data.isActive ? "true" : data.encouragingMessage}</p>
+         </div>
+         <div className="secondSlide">
+            <p>total nightly contribution: {data.nightTimeContributions}</p>
+            <p>coding at night message: {data.codingAtNightMessage}</p>
+         </div>
         </>
       )}
     </div>
-  );
+  )
 }
