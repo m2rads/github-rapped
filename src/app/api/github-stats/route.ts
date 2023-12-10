4a8d0b9ffcb2e954517476a6fa79bd72b067559d
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2023-12-09 18:47:50 -0800

    linting

--- src/app/api/github-stats/route.ts
+++ src/app/api/github-stats/route.ts
@@ -9,18 +9,18 @@ const slowStartMessages = [
   "Early in the year: coffee loading... ☕ Later: code compiling like a pro. That's an impressive runtime upgrade!",
   "January's commits were like searching for a semicolon; by mid-year, you're coding like it's a hackathon finale. Epic comeback!",
   "Started with 'Ctrl+C', evolved to 'Ctrl+V', now you're all about 'Ctrl+S'. Saving the day, one commit at a time!",
-  "First few months: coding at tortoise speed. Now? You're in the express lane on the JavaScript highway. Zooming past those bugs!"
+  "First few months: coding at tortoise speed. Now? You're in the express lane on the JavaScript highway. Zooming past those bugs!",
 ];
 
 const goodStartMessages = [
-  "You hit the ground running like a well-optimized algorithm in January! Keep up that high-performance computing! 💻🚀",
-  "Started the year with more commits than coffee breaks? Java-nice day! ☕👨‍💻",
+  'You hit the ground running like a well-optimized algorithm in January! Keep up that high-performance computing! 💻🚀',
+  'Started the year with more commits than coffee breaks? Java-nice day! ☕👨‍💻',
   "Your early-year coding spree? Simply 'for' loop-tastic! Keep iterating through success! 🔄🌟",
-  "Bug-free January? Looks like you’ve already debugged 2023! 🐞🎉",
+  'Bug-free January? Looks like you’ve already debugged 2023! 🐞🎉',
   "Pushing more than a gym enthusiast, you've got your code repo in shape early! 🏋️‍♂️💾",
   "You've been committing like it's a hackathon every day! Let's keep this code party going! 🎉👩‍💻",
-  "Array of hope! Your January contributions are like a perfectly indexed array, fast and efficient. Keep accessing those elements of success! 📈🤖"
-]
+  'Array of hope! Your January contributions are like a perfectly indexed array, fast and efficient. Keep accessing those elements of success! 📈🤖',
+];
 
 interface ContributionDay {
   contributionCount: number;
@@ -45,10 +45,8 @@ interface GitHubUser {
 }
 
 interface GraphQLResponse {
-    user: GitHubUser;
+  user: GitHubUser;
 }
-
-
 
 // const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN });
 // Initialize the GraphQL client with authentication
@@ -62,52 +60,67 @@ export async function POST(req: NextRequest) {
   const { username } = await req.json();
 
   if (typeof username !== 'string') {
-    return new NextResponse(JSON.stringify({ message: 'Username must be a string' }), {
-      status: 400,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    return new NextResponse(
+      JSON.stringify({ message: 'Username must be a string' }),
+      {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   }
 
   try {
     const monthlyCommits = await fetchCommitData(username);
     const yearlyCommits = monthlyCommits.reduce((a, b) => a + b, 0);
-    const averageMonthlyCommits = yearlyCommits / 12; 
-    const threshold = averageMonthlyCommits / 2; 
+    const averageMonthlyCommits = yearlyCommits / 12;
+    const threshold = averageMonthlyCommits / 2;
     const isActiveStart = monthlyCommits[0] >= threshold;
 
     let encouragingMessage = '';
     if (!isActiveStart) {
       // Check for months with increased activity
-      const activeMonths = monthlyCommits.map((commits, index) => commits > averageMonthlyCommits ? index : -1).filter(index => index > 0);
+      const activeMonths = monthlyCommits
+        .map((commits, index) => (commits > averageMonthlyCommits ? index : -1))
+        .filter((index) => index > 0);
       if (activeMonths.length > 0) {
         // Construct a message based on active months
-        encouragingMessage = slowStartMessages[Math.floor(Math.random() * slowStartMessages.length)];
+        encouragingMessage =
+          slowStartMessages[
+            Math.floor(Math.random() * slowStartMessages.length)
+          ];
       }
     } else {
-      encouragingMessage = goodStartMessages[Math.floor(Math.random() * goodStartMessages.length)];
+      encouragingMessage =
+        goodStartMessages[Math.floor(Math.random() * goodStartMessages.length)];
     }
 
-    return new NextResponse(JSON.stringify({
-      totalJanuaryCommits: monthlyCommits[0],
-      threshold: threshold,
-      isActiveStart: isActiveStart,
-      encouragingMessage: encouragingMessage
-    }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    return new NextResponse(
+      JSON.stringify({
+        totalJanuaryCommits: monthlyCommits[0],
+        threshold: threshold,
+        isActiveStart: isActiveStart,
+        encouragingMessage: encouragingMessage,
+      }),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   } catch (error) {
     console.error('Error accessing the GitHub API:', error);
-    return new NextResponse(JSON.stringify({ message: 'Internal Server Error' }), {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    return new NextResponse(
+      JSON.stringify({ message: 'Internal Server Error' }),
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   }
 }
 
@@ -135,13 +148,11 @@ async function fetchCommitData(username: string) {
         }
   `;
 
-
   // Fetch data
-  const result = await graphqlWithAuth(query, {
-    username: username
-  }) as GraphQLResponse;
+  const result = (await graphqlWithAuth(query, {
+    username: username,
+  })) as GraphQLResponse;
   // console.log("GraphQL query result:", result);
-
 
   // Process the result to calculate monthly commits
   let monthlyCommits = processIntoMonthlyContributions(result);
@@ -152,25 +163,32 @@ async function fetchCommitData(username: string) {
 function processIntoMonthlyContributions(data: GraphQLResponse): number[] {
   const monthlyContributions = new Array(12).fill(0);
 
-  console.log("total contribution: ", data.user.contributionsCollection.contributionCalendar.totalContributions)
+  console.log(
+    'total contribution: ',
+    data.user.contributionsCollection.contributionCalendar.totalContributions
+  );
 
-  data.user.contributionsCollection.contributionCalendar.weeks.forEach(week => {
-    week.contributionDays.forEach(day => {
-      const date = new Date(day.date);
-      const month = date.getMonth(); // 0-11
-      monthlyContributions[month] += day.contributionCount;
-    });
-  });
+  data.user.contributionsCollection.contributionCalendar.weeks.forEach(
+    (week) => {
+      week.contributionDays.forEach((day) => {
+        const date = new Date(day.date);
+        const month = date.getMonth(); // 0-11
+        monthlyContributions[month] += day.contributionCount;
+      });
+    }
+  );
 
-  console.log("let's see monthly contributions: ", monthlyContributions)
+  console.log("let's see monthly contributions: ", monthlyContributions);
 
   return monthlyContributions;
 }
 
-function countNightTimeContributions(weeksData: ContributionCalendar["weeks"]): number {
+function countNightTimeContributions(
+  weeksData: ContributionCalendar['weeks']
+): number {
   let nightTimeContributions = 0;
-  weeksData.forEach(week => {
-    week.contributionDays.forEach(day => {
+  weeksData.forEach((week) => {
+    week.contributionDays.forEach((day) => {
       const contributionDate = new Date(day.date);
       const hour = contributionDate.getUTCHours();
 
@@ -183,7 +201,6 @@ function countNightTimeContributions(weeksData: ContributionCalendar["weeks"]): 
 
   return nightTimeContributions;
 }
-
 
 // const result = await graphqlWithAuth(yourGraphQLQuery, {
 //   username: "yourUsername"
