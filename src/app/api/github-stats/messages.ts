52e607318913876371fdd7e574d7cb3c602cee5e
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-09 20:21:03 -0800

    cleanup code: moved extra code to their related files

---
+++ src/app/api/github-stats/messages.ts
@@ -0,0 +1,30 @@
+// some encouraging messages with added humor
+export const slowStartMessages = [
+    "Started the year on 'sleep mode', but then you hit 'Ctrl + Alt + Commit' – now you're on a coding spree!",
+    "Started the year on 'Hello, World!' but soon went full 'git commit -m 'Unleash the Code!'' mode. That's some serious version control magic!",
+    "You began the year on 'debug mode', but then switched to 'release mode' with style. Keep on compiling those wins!",
+    "Early in the year: coffee loading... ☕ Later: code compiling like a pro. That's an impressive runtime upgrade!",
+    "January's commits were like searching for a semicolon; by mid-year, you're coding like it's a hackathon finale. Epic comeback!",
+    "Started with 'Ctrl+C', evolved to 'Ctrl+V', now you're all about 'Ctrl+S'. Saving the day, one commit at a time!",
+    "First few months: coding at tortoise speed. Now? You're in the express lane on the JavaScript highway. Zooming past those bugs!"
+  ];
+  
+export const goodStartMessages = [
+    "You hit the ground running like a well-optimized algorithm in January! Keep up that high-performance computing! 💻🚀",
+    "Started the year with more commits than coffee breaks? Java-nice day! ☕👨‍💻",
+    "Your early-year coding spree? Simply 'for' loop-tastic! Keep iterating through success! 🔄🌟",
+    "Bug-free January? Looks like you’ve already debugged 2023! 🐞🎉",
+    "Pushing more than a gym enthusiast, you've got your code repo in shape early! 🏋️‍♂️💾",
+    "You've been committing like it's a hackathon every day! Let's keep this code party going! 🎉👩‍💻",
+    "Array of hope! Your January contributions are like a perfectly indexed array, fast and efficient. Keep accessing those elements of success! 📈🤖"
+  ]
+  
+export const codingAtNightMessages = [
+    "You're the superhero of the dark mode, turning night into a code-filled delight! Keep lighting up the IDE! 🌙⌨️✨",
+    "While the world sleeps, you merge and commit. The night is your domain, the moon your pair programming partner! 🌕👩‍💻",
+    "Owls hoot, bats fly, and you... code? Night is your canvas, and every line of code is a star in your sky! 🦉🌌",
+    "You code as if the night has a deadline, and sunrise is the client. Shine on, you nocturnal developer! 🌃💻",
+    "Forget counting sheep, you count loops and functions. Your code dreams are the stuff of legends! 🐑🔄",
+    "Pull requests by day, push commits by night. You're the guardian of the repository, the keeper of the code! 🛡️🌟",
+    "Coding at night? That's when the real magic happens, and the bugs fear to tread. You're the wizard of the witching hour! 🧙‍♂️🔮"
+  ]
--- src/app/api/github-stats/route.ts
+++ src/app/api/github-stats/route.ts
@@ -1,67 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { graphql } from '@octokit/graphql';
+import { ContributionCalendar, GraphQLResponse } from './types' 
+import { slowStartMessages, goodStartMessages, codingAtNightMessages } from './messages'
 
-// some encouraging messages with added humor
-const slowStartMessages = [
-  "Started the year on 'sleep mode', but then you hit 'Ctrl + Alt + Commit' – now you're on a coding spree!",
-  "Started the year on 'Hello, World!' but soon went full 'git commit -m 'Unleash the Code!'' mode. That's some serious version control magic!",
-  "You began the year on 'debug mode', but then switched to 'release mode' with style. Keep on compiling those wins!",
-  "Early in the year: coffee loading... ☕ Later: code compiling like a pro. That's an impressive runtime upgrade!",
-  "January's commits were like searching for a semicolon; by mid-year, you're coding like it's a hackathon finale. Epic comeback!",
-  "Started with 'Ctrl+C', evolved to 'Ctrl+V', now you're all about 'Ctrl+S'. Saving the day, one commit at a time!",
-  "First few months: coding at tortoise speed. Now? You're in the express lane on the JavaScript highway. Zooming past those bugs!"
-];
-
-const goodStartMessages = [
-  "You hit the ground running like a well-optimized algorithm in January! Keep up that high-performance computing! 💻🚀",
-  "Started the year with more commits than coffee breaks? Java-nice day! ☕👨‍💻",
-  "Your early-year coding spree? Simply 'for' loop-tastic! Keep iterating through success! 🔄🌟",
-  "Bug-free January? Looks like you’ve already debugged 2023! 🐞🎉",
-  "Pushing more than a gym enthusiast, you've got your code repo in shape early! 🏋️‍♂️💾",
-  "You've been committing like it's a hackathon every day! Let's keep this code party going! 🎉👩‍💻",
-  "Array of hope! Your January contributions are like a perfectly indexed array, fast and efficient. Keep accessing those elements of success! 📈🤖"
-]
-
-const codingAtNightMessages = [
-  "You're the superhero of the dark mode, turning night into a code-filled delight! Keep lighting up the IDE! 🌙⌨️✨",
-  "While the world sleeps, you merge and commit. The night is your domain, the moon your pair programming partner! 🌕👩‍💻",
-  "Owls hoot, bats fly, and you... code? Night is your canvas, and every line of code is a star in your sky! 🦉🌌",
-  "You code as if the night has a deadline, and sunrise is the client. Shine on, you nocturnal developer! 🌃💻",
-  "Forget counting sheep, you count loops and functions. Your code dreams are the stuff of legends! 🐑🔄",
-  "Pull requests by day, push commits by night. You're the guardian of the repository, the keeper of the code! 🛡️🌟",
-  "Coding at night? That's when the real magic happens, and the bugs fear to tread. You're the wizard of the witching hour! 🧙‍♂️🔮"
-]
-
-interface ContributionDay {
-  contributionCount: number;
-  date: string;
-}
-
-interface ContributionWeek {
-  contributionDays: ContributionDay[];
-}
-
-interface ContributionCalendar {
-  totalContributions: number;
-  weeks: ContributionWeek[];
-}
-
-interface ContributionsCollection {
-  contributionCalendar: ContributionCalendar;
-}
-
-interface GitHubUser {
-  contributionsCollection: ContributionsCollection;
-}
-
-interface GraphQLResponse {
-    user: GitHubUser;
-}
-
-
-
-// const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN });
-// Initialize the GraphQL client with authentication
 const graphqlWithAuth = graphql.defaults({
   headers: {
     authorization: `token ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
@@ -199,38 +140,3 @@ function countNightTimeContributions(weeksData: ContributionCalendar["weeks"]): 
 
   return nightTimeContributions;
 }
-
-
-// const result = await graphqlWithAuth(yourGraphQLQuery, {
-//   username: "yourUsername"
-// });
-
-// const monthlyContributions = processIntoMonthlyContributions(result);
-
-// github restful api implementation
-// async function fetchCommitData(username: string) {
-//   const currentYear = new Date().getFullYear();
-//   const startOfYear = new Date(currentYear, 0, 1).toISOString();
-//   const endOfYear = new Date(currentYear + 1, 0, 0).toISOString();
-
-//   const yearlyCommitsData = await getCommitsCount(username, startOfYear, endOfYear);
-
-//   // Process yearly data to calculate commits per month
-//   let monthlyCommits = processYearlyDataIntoMonthly(yearlyCommitsData, currentYear);
-
-//   return monthlyCommits;
-// }
-
-// async function getCommitsCount(username: string, startDate: string, endDate: string): Promise<any[]> {
-//   // Fetch the commit data for the specified date range
-//   // Handle pagination if necessary
-
-//   const commitsResponse = await octokit.request('GET /search/commits', {
-//     q: `author:${username} committer-date:${startDate}..${endDate}`,
-//     sort: 'committer-date',
-//     order: 'asc',
-//     per_page: 100 // Adjust for pagination
-//   });
-
-//   return commitsResponse.data.items; // Access the items array
-// }
