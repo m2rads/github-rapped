0a57df652dd7d3c068dcac4432e2846b6b44b82b
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-17 18:58:22 -0800

    fix: icon import structure in page.tsx

--- src/app/wrapped/page.tsx
+++ src/app/wrapped/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { createRef, useRef, useState } from 'react';
+import { RightArrowIcon, LeftArrowIcon, PauseIcon, PlayIcon } from './Icons';
 import { SliderIndicator } from '@/app/wrapped/SliderIndicator';
 import { Slide } from '@/app/wrapped/types';
 
@@ -87,67 +88,5 @@ export default function WrappedPage() {
         </div>
       </div>
     </div>
-  );
-}
-
-export function RightArrowIcon() {
-  return (
-    <svg
-      data-encore-id='icon'
-      fill={'white'}
-      role='img'
-      aria-hidden='true'
-      viewBox='0 0 24 24'
-      className='Svg-sc-ytk21e-0 fviPxI'
-    >
-      <path d='M10.228 7.293a1 1 0 0 0 0 1.414L13.52 12l-3.293 3.293a1 1 0 1 0 1.414 1.414L16.349 12l-4.707-4.707a1 1 0 0 0-1.414 0z'></path>
-      <path d='M21 12a9 9 0 1 0-18 0 9 9 0 0 0 18 0zM12 1c6.075 0 11 4.925 11 11s-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1z'></path>
-    </svg>
-  );
-}
-
-export function LeftArrowIcon() {
-  return (
-    <svg
-      data-encore-id='icon'
-      fill={'white'}
-      role='img'
-      aria-hidden='true'
-      viewBox='0 0 24 24'
-      className='Svg-sc-ytk21e-0 fviPxI'
-    >
-      <path d='M13.772 7.293a1 1 0 0 1 0 1.414L10.48 12l3.293 3.293a1 1 0 0 1-1.414 1.414L7.651 12l4.707-4.707a1 1 0 0 1 1.414 0z'></path>
-      <path d='M3 12a9 9 0 1 1 18 0 9 9 0 0 1-18 0zm9-11C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1z'></path>
-    </svg>
-  );
-}
-
-export function PauseIcon() {
-  return (
-    <svg
-      data-encore-id='icon'
-      fill={'white'}
-      role='img'
-      aria-hidden='true'
-      viewBox='0 0 24 24'
-      className='Svg-sc-ytk21e-0 fLlswU'
-    >
-      <path d='M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z'></path>
-    </svg>
-  );
-}
-
-export function PlayIcon() {
-  return (
-    <svg
-      data-encore-id='icon'
-      fill={'white'}
-      role='img'
-      aria-hidden='true'
-      viewBox='0 0 24 24'
-      className='Svg-sc-ytk21e-0 fLlswU'
-    >
-      <path d='m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z'></path>
-    </svg>
   );
 }
